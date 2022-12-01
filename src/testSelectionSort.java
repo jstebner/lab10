3985f713df9ef84ca7a6de546ccc953bb1593ca4
jong <jstebner007@gmail.com>
2022-12-01 12:23:10 -0500

    setup

---
+++ src/testSelectionSort.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class testSelectionSort {
+    @Test
+    public void test() {
+        testPositives();
+        testNegatives();
+        testMixed();
+        testDuplicates();
+    }
+    // 1) testPositives – testing a list of all positive integers
+    public void testPositives() {
+
+    }
+    // 2) testNegatives – testing a list of all negative integers
+    public void testNegatives() {
+
+    }
+    // 3) testMixed – testing a list containing positive, negative and zeros.
+    public void testMixed() {
+
+    }
+    // 4) testDuplicates  –  testing  a  list  containing  one  or  more  duplicate 
+    //                       number, for both positive and negative numbers
+    public void testDuplicates() {
+        
+    }
+}
